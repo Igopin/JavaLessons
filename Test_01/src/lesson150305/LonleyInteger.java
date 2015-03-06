20e669ea24b7c8f46d5995a7cff9d9e1906843b5
Igor Pinaev <igopin@gmail.com>
2015-03-06 19:00:10 +0300

    Another lesson

---
+++ Test_01/src/lesson150305/LonleyInteger.java
@@ -0,0 +1,41 @@
+package lesson150305;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class LonleyInteger {
+	
+	static int lonelyinteger(int[] a) {
+		return 0;
+    }
+	
+	
+	private static void maxXor(int l, int r) {
+        
+		int tmp, max = 0;
+		for (int i = l; i <= r; i++){
+			for (int j = l; j <= r; j++) {				
+				if (i > j) {
+	                continue;
+                }
+				if ((tmp = i^j) > max) {
+	                max = tmp;
+                } 	            
+            }
+		}
+		System.out.println(max);
+	}
+
+	public static void main(String[] args) {
+		int a[] = new int [10];
+		
+		System.out.println(a[7]);
+        int l = 10, r = 15;
+		maxXor(l, r);
+		
+	}
+
+}
--- Test_01/src/test_01/Hello.java
+++ Test_01/src/test_01/Hello.java
@@ -1,5 +1,22 @@
 package test_01;
 
+import java.util.Scanner;
+
 public class Hello {
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		while (scanner.hasNextLine()){
+			String nextLine = scanner.nextLine();
+			System.out.println(nextLine);
+			
+			if (nextLine.equals("exit")) {
+				break;
+			}
+		}
+		
+		System.out.println("Good bye!");
+	}
 
 }
